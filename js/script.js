0f020bfb7bc919974aeedbf8208e23808fca6c10
Soumeya <soumeyahassan47@gmail.com>
2021-04-07 21:34:51 +0100

    local storage city names

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+const favCitiesArray = [];
 const saveFavCityName = (event) => {
   const buttonTarget = $(event.currentTarget);
 
   if (buttonTarget.is("button")) {
-    console.log("hello");
+    const cityName = buttonTarget.parent().find("input").val();
+
+    const cityNameObject = {
+      cityName,
+    };
+
+    favCitiesArray.push(cityNameObject);
+
+    console.log(favCitiesArray);
+    localStorage.setItem("favoriteCities", JSON.stringify(favCitiesArray));
   }
 };
 $("#searchBtn").click(saveFavCityName);
